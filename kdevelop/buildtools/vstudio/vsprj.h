7263973c2ce2b6266d578a6699460fa31aef5d04
Iegor <rmtdev@gmail.com>
2013-04-03 11:24:06 +0000

    added vstudio-buildtools files.

---
+++ kdevelop/buildtools/vstudio/vsprj.h
@@ -0,0 +1,11 @@
+//
+// C++ Interface: vsprj
+//
+// Description: 
+//
+//
+// Author: iegor <rmtdev@gmail.com>, (C) 2013
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
